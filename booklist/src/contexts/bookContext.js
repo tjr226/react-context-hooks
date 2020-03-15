876d2360bb1bba6bc4cd1f4dc0265e4b73da18a3
tjr226 <tjr226@gmail.com>
2020-03-15 12:14:36 -0400

    tutorial complete, added reducers and localstorage to final booklist project

--- booklist/src/contexts/bookContext.js
+++ booklist/src/contexts/bookContext.js
@@ -1,25 +1,18 @@
-import React, { createContext, useState } from 'react';
-import {v1 as uuid} from 'uuid';
+import React, { createContext, useReducer, useEffect } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
-        {title: 'the final empire', author: 'brandon sanderson', id: 2},
-
-    ])
-
-    const addBook = (title, author) => {
-        setBooks([...books, {title, author, id: uuid() }])
-    }
-
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id))
-    };
-
+    const [books, dispatch] = useReducer(bookReducer, [], () => {
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : [];
+    });
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books))
+    }, [books])
     return (
-        <BookContext.Provider value={{books, addBook, removeBook}}>
+        <BookContext.Provider value={{books, dispatch}}>
             { props.children }
         </BookContext.Provider>
     )
